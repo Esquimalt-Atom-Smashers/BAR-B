73076f8576e22c398707abc6e958ce0e39d44291
12368 <isaac@trevorandterri.com>
2023-12-03 22:56:06 -0800

    Deleted  code that isnt used for teleop swerve

--- src/main/java/frc/lib/vision/LimelightRawAngles.java
+++
@@ -1,8 +0,0 @@
-package frc.lib.vision;
-
-
- public record LimelightRawAngles(double tx, double ty, double ta) {
-    public LimelightRawAngles(double tx, double ty) {
-        this(ty, tx, 0.0);
-    }
- }
